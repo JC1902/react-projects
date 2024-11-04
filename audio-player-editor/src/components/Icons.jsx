1b5727d12cfbb474d800fd56efe6431753f17e49
JC1902 <jorgecisneros2002@gmail.com>
2024-11-04 11:29:29 -0600

    First commit, player almost ready, beginning equalizer

---
+++ audio-player-editor/src/components/Icons.jsx
@@ -0,0 +1,35 @@
+export const PlayIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    aria-hidden="true"
+    className="w-full h-full text-gray-800 dark:text-white"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M8.6 5.2A1 1 0 0 0 7 6v12a1 1 0 0 0 1.6.8l8-6a1 1 0 0 0 0-1.6l-8-6Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
+
+export const PauseIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={24}
+    height={24}
+    fill="currentColor"
+    aria-hidden="true"
+    className="w-full h-full text-gray-800 dark:text-white"
+    {...props}
+  >
+    <path
+      fillRule="evenodd"
+      d="M8 5a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2H8Zm7 0a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2h-1Z"
+      clipRule="evenodd"
+    />
+  </svg>
+)
